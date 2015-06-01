9d25a8be1b901315d23a3e29aff1b646c43b1da7
wenchao zheng <wen@Wenchaos-MacBook-Air.local>
2015-06-01 17:25:01 +0800

    Initial Commit

---
+++ DelegateDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DelegateDemo
+//
+//  Created by Wenchao Zheng on 2015-06-01.
+//  Copyright (c) 2015 Wenchao Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
